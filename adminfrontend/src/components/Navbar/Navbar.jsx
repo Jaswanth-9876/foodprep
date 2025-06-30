4c502e39d869e59040bed33e09f093c0832ed815
Jaswanth-9876 <jaswanthsathuluri@gmail.com>
2025-06-30 13:43:33 +0530

    Updated admin UI components

--- adminfrontend/src/components/Navbar/Navbar.jsx
+++ adminfrontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import {assets} from '../../assets/assets'
 import './Navbar.css'
-
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <img className='logo' src={assets.logo} alt="" />
+        <img src={assets.profile_image} alt="" className="profile" />
+    </div>
   )
 }
 
--- adminfrontend/src/components/Sidebar/Sidebar.jsx
+++ adminfrontend/src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+import {assets} from '../../assets/assets'
 import './Sidebar.css'
-
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='sidebar'>
+        <div className="sidebar-options">
+            <NavLink to='/add'  className="sidebar-option">
+                <img src={assets.add_icon} alt="" />
+                <p>Add Items</p>
+            </NavLink>
+            <NavLink to='/list' className="sidebar-option">
+                <img src={assets.list_icon} width="27px" alt="" />
+                <p>List Items</p>
+            </NavLink>
+            <NavLink to='/orders'  className="sidebar-option">
+                <img src={assets.order_icon} alt="" />
+                <p>Orders</p>
+            </NavLink>
+        </div>
+    </div>
   )
 }
 
